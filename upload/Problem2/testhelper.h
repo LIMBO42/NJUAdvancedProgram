4df34ad7947f2437261011db6a1e8b17319cd232
liusy58 <liusy58@smail.nju.edu.cn>
2021-09-18 09:35:18 +0800

    modify main.cpp and tester.cpp

--- upload/Problem2/testhelper.h
+++ upload/Problem2/testhelper.h
@@ -30,7 +30,7 @@ namespace test_helper {
     alloc_trace_enabled = false;
     std::size_t ret;
     if (alloc_size.find(ptr) == alloc_size.end()) {
-      std::printf("*** Detected invalid memory free for address %p ***\n", ptr);
+      // std::printf("*** Detected invalid memory free for address %p ***\n", ptr);
       assert(false && "Invalid memory free");
     }
     ret = alloc_size[ptr];
@@ -58,22 +58,23 @@ namespace test_helper {
 
   bool run_test(const std::function<void(void)> func) {
     static int case_counter = 1;
-    printf("Case %d:\n", case_counter++);
+    // printf("Case %d:\n", case_counter++);
     test_has_errors = false;
     alloc_mem = 0;
     alloc_times = 0;
     func();
-    if (test_has_errors) {
-      printf("FAIL!\n");
-    } else {
-      printf("Pass.\n");
-    }
+    // if (test_has_errors) {
+    //   // printf("FAIL!\n");
+    // } else {
+    //   // printf("Pass.\n");
+    // }
+    printf("%d\n",test_has_errors);
     return test_has_errors;
   }
 
   bool test_assert(int line_no, bool stat, const char * err_message) {
     if (!stat) {
-      printf("Assertion failed (LINE %d): %s\n", line_no, err_message);
+      //printf("Assertion failed (LINE %d): %s\n", line_no, err_message);
       test_has_errors = true;
     }
     return stat;
