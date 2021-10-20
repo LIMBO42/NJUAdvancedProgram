b617041b9c714b9a56c1913eae66f4452e8a451f
liusy58 <liusy58@smail.nju.edu.cn>
2021-10-20 21:39:35 +0800

    modify handout

--- handout/hashmap.cpp
+++ handout/hashmap.cpp
@@ -1,50 +1,81 @@
-#include "hashmap.hpp"
 
+#include "hashmap.hpp"
 template <typename K, typename M, typename H>
 HashMap<K, M, H>::HashMap() : HashMap(kDefaultBuckets) { }
 
 template <typename K, typename M, typename H>
-HashMap<K, M, H>::HashMap(size_t bucket_count, const H& hash ) { }
+HashMap<K, M, H>::HashMap(size_t bucket_count, const H& hash) :
+    _size(0),
+    _hash_function(hash),
+    _buckets_array(bucket_count, nullptr) {}
 
 template <typename K, typename M, typename H>
-HashMap<K, M, H>::~HashMap() {}
+HashMap<K, M, H>::~HashMap() {
+    //to do 
+}
 
 template <typename K, typename M, typename H>
-inline size_t HashMap<K, M, H>::size() const noexcept {return {};}
+inline size_t HashMap<K, M, H>::size() const noexcept {
+    //to do
+    return 0;
+}
 
 template <typename K, typename M, typename H>
-inline bool HashMap<K, M, H>::empty() const noexcept {return {};}
+inline bool HashMap<K, M, H>::empty() const noexcept {
+    //todo
+    return true;
+}
 
 template <typename K, typename M, typename H>
-inline float HashMap<K, M, H>::load_factor() const noexcept {return {};};
+inline float HashMap<K, M, H>::load_factor() const noexcept {
+    //to do
+    return 0;
+};
 
 template <typename K, typename M, typename H>
-size_t HashMap<K, M, H>::bucket_count() const noexcept {return {};};
+size_t HashMap<K, M, H>::bucket_count() const noexcept {
+    //to do
+    return 0;
+};
 
 template <typename K, typename M, typename H>
-bool HashMap<K, M, H>::contains(const K& key) const noexcept {return {};}
+bool HashMap<K, M, H>::contains(const K& key) const noexcept {
+    //to do 
+    return true;
+}
 
 template <typename K, typename M, typename H>
-void HashMap<K, M, H>::clear() noexcept {return {};}
+void HashMap<K, M, H>::clear() noexcept {
+    //to do
+  
+}
 
 template <typename K, typename M, typename H>
 std::pair<typename HashMap<K, M, H>::value_type*, bool>
-HashMap<K, M, H>::insert(const value_type& value) {return {};}
+HashMap<K, M, H>::insert(const value_type& value) {
+    //to do
+    return { new value_type{ },true };
+}
 
 
 template <typename K, typename M, typename H>
-bool HashMap<K, M, H>::erase(const K& key) {return {};}
+bool HashMap<K, M, H>::erase(const K& key) {
+    //to do
+    return true;
+}
 
 template <typename K, typename M, typename H>
-M& HashMap<K, M, H>::at(const K& key)const{return {};}
+M& HashMap<K, M, H>::at(const K& key)const {
+    return * (new M());
+}
 
 template <typename K, typename M, typename H>
 void HashMap<K, M, H>::debug() const {
     std::cout << std::setw(30) << std::setfill('-') << '\n' << std::setfill(' ')
-              << "Printing debug information for your HashMap implementation\n"
-              << "Size: " << size() << std::setw(15) << std::right
-              << "Buckets: " << bucket_count() << std::setw(20) << std::right
-              << "(load factor: " << std::setprecision(2) << load_factor() << ") \n\n";
+        << "Printing debug information for your HashMap implementation\n"
+        << "Size: " << size() << std::setw(15) << std::right
+        << "Buckets: " << bucket_count() << std::setw(20) << std::right
+        << "(load factor: " << std::setprecision(2) << load_factor() << ") \n\n";
 
     for (size_t i = 0; i < bucket_count(); ++i) {
         std::cout << "[" << std::setw(3) << i << "]:";
@@ -52,39 +83,65 @@ void HashMap<K, M, H>::debug() const {
         while (curr != nullptr) {
             const auto& [key, mapped] = curr->value;
             // next line will not compile if << not supported for K or M
-            std::cout <<  " -> " << key << ":" << mapped;
+            std::cout << " -> " << key << ":" << mapped;
             curr = curr->next;
         }
-        std::cout <<  " /" <<  '\n';
+        std::cout << " /" << '\n';
     }
     std::cout << std::setw(30) << std::setfill('-') << '\n';
 }
 
 
 
-template <typename K, typename M, typename H>
-typename HashMap<K, M, H>::node_pair HashMap<K, M, H>::find_node(const K& key) const {return {};}
 
 template <typename K, typename M, typename H>
-void HashMap<K, M, H>::rehash(size_t new_bucket_count) {return {};}
+typename HashMap<K, M, H>::node_pair HashMap<K, M, H>::find_node(const K& key) const {
+    //to do
+    return { nullptr,nullptr };
+}
 
 template <typename K, typename M, typename H>
-M& HashMap<K, M, H>::operator[](const K& key){return {};}
+void HashMap<K, M, H>::rehash(size_t new_bucket_count) {
+    //to do
+}
+
 
 template <typename K, typename M, typename H>
-std::ostream& operator<<(std::ostream& os, const HashMap<K, M, H>& map){return {};}
+M& HashMap<K, M, H>::operator[](const K& key) {
+    //to do
+    return * (new M());
+}
+
+template <typename K, typename M, typename H>
+std::ostream& operator<<(std::ostream& os, const HashMap<K, M, H>& map) {
+    return os;
+}
 
 template <typename K, typename M, typename H>
 bool operator==(const HashMap<K, M, H>& lhs,
-                const HashMap<K, M, H>& rhs){return {};}
+    const HashMap<K,M,H>& rhs)
+{
+    //to do
+    return true;
+}
 
 template <typename K, typename M, typename H>
 bool operator!=(const HashMap<K, M, H>& lhs,
-                const HashMap<K, M, H>& rhs){return {};}
+    const HashMap<K, M, H>& rhs) {
+    //to do
+    return true;
+}
 
 template <typename K, typename M, typename H>
-HashMap<K, M, H>::HashMap(HashMap const &other){return {};}
+HashMap<K, M, H>::HashMap(HashMap const& other) {
+    //to do
+    return;
+}
 
 
 template<typename K, typename M, typename H>
-HashMap<K,M,H> &HashMap<K, M, H>::operator=(const HashMap &other) {return {};}
+HashMap<K, M, H>& HashMap<K, M, H>::operator=(const HashMap& other) {
+    //to do
+    return *this;
+}
+
