ef47a4091f1802ed2024c79b4241585e90fe7863
LIMBO_42 <1541902043@qq.com>
2021-11-03 18:15:07 +0800

    Update iterator.hpp

--- handout/iterator.hpp
+++ handout/iterator.hpp
@@ -13,16 +13,33 @@ class HashMap<K,M,H>::iterator:public std::iterator<std::input_iterator_tag,valu
         node*curr_node= nullptr;
     public:
         iterator(const HashMap*mp,bool end);
-        iterator(const iterator&it);
+        iterator(const iterator&it){
+            hashMap = it.hashMap;
+            index = it.index;
+            is_end = it.is_end;
+            curr_node = it.curr_node;
+        }
         friend bool operator==(const iterator& lhs,const iterator& rhs);
-        friend bool operator!=(const iterator& lhs,const iterator& rhs);
-        value_type& operator*()const;
+        friend bool operator!=(const iterator& lhs,const iterator& rhs){
+            return !(lhs == rhs);
+        }
+        value_type& operator*()const{
+            
+        }
         iterator&operator++();
         iterator operator++(int);
         iterator&operator=(iterator&other);
         iterator&operator=(const iterator&other);
-        value_type*operator->() const;
-        bool key_equal(const K&key);
+        value_type*operator->() const{
+            if(curr_node == nullptr)
+                throw "error!";
+            return &(curr_node->value);
+        }
+        bool key_equal(const K&key){
+            if(is_end||curr_node== nullptr)
+                return false;
+            return curr_node->value.first == key;
+        }
     };
 
 
